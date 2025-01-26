a0049b463b5a2152d28ae3589653a609493ebc6b
Lasse <62940398+lassenym@users.noreply.github.com>
2025-01-27 00:59:01 +0100

    version 2.1
    added info panel
    streamlined some processes
    fixed bugs

--- pacman_helper.py
+++ pacman_helper.py
@@ -1,4 +1,4 @@
-import sqlite3, os, sys
+import sqlite3, os, sys, shutil, re, subprocess
 import pyalpm
 
 RED = "\033[91m"
@@ -17,6 +17,10 @@ def import_packages():
     db = handle.get_localdb()
     return db.pkgcache
 
+def import_db():
+    handle = pyalpm.Handle("/", "/var/lib/pacman")
+    return handle.get_localdb()
+
 def db_connect():
     conn = sqlite3.connect(os.path.join(os.path.dirname(os.path.abspath(__file__)), FILENAME))
     return conn, conn.cursor()
@@ -32,8 +36,7 @@ def db_make():
     );
     """)
 
-    pkgs = import_packages()
-    for pkg in pkgs:
+    for pkg in import_packages():
         if pkg.reason == 0:
             cursor.execute("INSERT INTO packages (name, desc, flag) VALUES (?, ?, ?);", (pkg.name, pkg.desc, "x"))
         else:
@@ -84,10 +87,11 @@ def db_sort():
 
 def pkg_sort(name, desc):
     conn, cursor = db_connect()
+
     print(f"'{RED}{name}{RESET}' needs to be sorted: {BLACK}({desc}){RESET}")
-    
     new_flag = input(f"'{BLUE}s{RESET}', '{GREEN}p{RESET}' or '{YELLOW}l{RESET}'? ({BLUE}system{RESET}, {GREEN}program{RESET} or {YELLOW}library{RESET}): ")
-    if new_flag not in ('s', 'p', 'l'):
+
+    if new_flag not in ['s', 'p', 'l']:
         print(f"\n{RED}Error: Wrong flag. Please enter given flags.{RESET}\n")
     else:
         cursor.execute("UPDATE packages SET flag = ? WHERE name = ?;", (new_flag, name))
@@ -101,7 +105,8 @@ def db_print(cat):
     's': 'System Packages',
     'p': 'Programs',
     'l': 'Libraries',
-    'a': 'All Packages'}
+    'a': 'All Packages'
+    }
     conn, cursor = db_connect()
 
     if cat == 'a':
@@ -113,17 +118,95 @@ def db_print(cat):
         return
     conn.close()
 
-    if cat in CATEGORIES:
-        print(f"{YELLOW}{CATEGORIES[cat]}:{RESET}\n{BEIGE}--------------------------------------------------------{RESET}")
-        for pkg in pkgs:
-            print(f"{BLUE}{pkg[0]}{RESET} {BLACK}({pkg[1]}){RESET}")
-        print(f"{BEIGE}--------------------------------------------------------{RESET}")
-        print(f"{GRAYB}{YELLOW}{len(pkgs)}{RESET}{GRAYB} packages in category '{YELLOW}{CATEGORIES[cat]}{RESET}{GRAYB}' installed{RESET}")
+    print(f"{YELLOW}{CATEGORIES[cat]}:{RESET}\n{BEIGE}--------------------------------------------------------{RESET}")
+    for pkg in pkgs:
+        print(f"{BLUE}{pkg[0]}{RESET} {BLACK}({pkg[1]}){RESET}")
+    print(f"{BEIGE}--------------------------------------------------------{RESET}")
+    print(f"{GRAYB}{YELLOW}{len(pkgs)}{RESET}{GRAYB} packages in category '{YELLOW}{CATEGORIES[cat]}{RESET}{GRAYB}' installed{RESET}")
 
+def get_pkg_from_name(name):
+    name = re.sub(r"[<>=!~]+\s*\d+(\.\d+)*.*", "", name).strip()
+
+    if import_db().get_pkg(name):
+        return import_db().get_pkg(name)
+
+    for pkg in import_packages():
+        for provided in pkg.provides:
+            if provided.startswith(name):
+                return pkg
+
+def get_biggest_pkgs(count):
+    packages = [(pkg.name, int(round(pkg.isize / (1024 * 1024), 0))) for pkg in import_packages()]
+    packages = sorted(packages, key=lambda x: float(x[1]), reverse=True)
+
+    return packages[:count]
+
+def get_last_pkgs(count):
+    log_file = "/var/log/pacman.log"
+    package_names = [pkg.name for pkg in import_packages()]
+    package_history = {}
+
+    with open(log_file, 'r') as log:
+        for line in log:
+            if " installed " in line:
+                if line.split(" ")[3] in package_names and not import_db().get_pkg(line.split(" ")[3]).reason:
+                    package_history[line.split(" ")[3]] = line.split(" ")[0][1:-6]
+
+    return sorted(package_history.items(), key=lambda x: x[1], reverse=True)[:count]
+
+def get_most_pkgs(count):
+    pkg_count = []
+    for pkg in import_packages():
+        pkg_count.append([pkg.name, len(pkg.depends)])
+    
+    pkg_count.sort(key=lambda x: x[1], reverse=True)
+    
+    return pkg_count[:count]
+
+def print_info():
+    rows = 10
+    columns = 3
+    space_per_row = int(shutil.get_terminal_size()[0] / columns) - 1
+
+    biggest = get_biggest_pkgs(rows)
+    last  = get_last_pkgs(rows)
+    most_dependencies = get_most_pkgs(rows)
+
+    print(f"\n{YELLOW}{("BIGGEST PACKAGES:"[:space_per_row+1])}{RESET}{(" " * (space_per_row - 16))}|", end="")
+    print(f"{YELLOW}{("LAST INSTALLED:"[:space_per_row+1])}{RESET}{(" " * (space_per_row - 14))}|", end="")
+    print(f"{YELLOW}{("MOST DEPENDENCIES:"[:space_per_row])}{RESET}")
+
+
+    print("-" * shutil.get_terminal_size()[0])
+
+    for i in range(rows):
+        output = f"{RED}{str(biggest[i][1])[:space_per_row]} MB {BLUE}{biggest[i][0][:space_per_row - len(str(biggest[i][1])) - 3]}{RESET}"
+        output += " " * (space_per_row - len(biggest[i][0][:space_per_row]) - len(str(biggest[i][1])[:space_per_row - len(biggest[i][0])]) - 3) + "|"
+        
+        output += f"{RED}{last[i][1][:10]} {BLUE}{last[i][0][:space_per_row - len(last[i][1][:10])]}{RESET}"
+        output += " " * (space_per_row - len(last[i][0][:space_per_row - 1]) - len(last[i][1][:10])) + "|"
+
+        output += f"{RED}{str(most_dependencies[i][1])[:space_per_row]} {BLUE}{most_dependencies[i][0][:space_per_row - len(str(most_dependencies[i][1]))]}{RESET}"
+
+        print(output)
+
+    print("-" * shutil.get_terminal_size()[0])
+
+    for pkg in import_packages():
+        if len(pkg.compute_requiredby()) == 0 and pkg.reason:
+            if len(pkg.compute_optionalfor()) == 1:
+                print(f"{YELLOW}{pkg.name}{RESET} is not required. It is optional for: {BLUE}{pkg.compute_optionalfor()[0]}{RESET}")
+            elif len(pkg.compute_optionalfor()) == 2:
+                print(f"{YELLOW}{pkg.name}{RESET} is not required. It is optional for: {BLUE}{pkg.compute_optionalfor()[0]}{RESET} and {BLUE}{pkg.compute_optionalfor()[1]}{RESET}")
+            else:
+                print(f"{YELLOW}{pkg.name}{RESET} is not required. It is optional for: {BLUE}{pkg.compute_optionalfor()}{RESET}")
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        db_print(sys.argv[1])
+        if sys.argv[1] == "i":
+            print_info()
+        else:
+            db_print(sys.argv[1])
     else:
         if not os.path.isfile(os.path.join(os.path.join(os.path.dirname(__file__)), FILENAME)):
             db_make()
